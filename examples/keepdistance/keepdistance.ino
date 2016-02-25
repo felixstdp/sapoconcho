6c6662b0192c52c92ae581fe652872264666738e
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-02-25 11:53:44 +0100

    Update keepdistance.ino

--- examples/keepdistance/keepdistance.ino
+++ examples/keepdistance/keepdistance.ino
@@ -1,3 +1,6 @@
+#include <L9110.h>
+L9110 sapoconcho;
+
 #include <NewPing.h>
 
 #define TRIGGER_PIN  7  // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -6,21 +9,7 @@
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
-// programa básico para Sapoconcho con los movimientos como subrutinas y no como librerías
-
-#define AIA 11 // HIGH >> left backward
-#define AIB 6 // HIGH >> left forward
-#define BIA 9 // HIGH >> right forward
-#define BIB 10 // HIGH >> right backwards
-
-void setup() {
-
-  pinMode(AIA,OUTPUT);
-  pinMode(AIB,OUTPUT);
-  pinMode(BIA,OUTPUT);
-  pinMode(BIB,OUTPUT);
-  
-}
+void setup() {}
 
 void loop() {
 
@@ -30,27 +19,9 @@ void loop() {
   if (d==0) d=50;
  
   if (d<20) {
-    forward(120,120,50);
+    sapoconcho.forward(120,120,50);
   } else {
-    reverse(120,120,50);
+    sapoconcho.reverse(120,120,50);
   }
     
-}
-
-void forward(int l, int r, int t)
-{
-  analogWrite(AIA,0);
-  analogWrite(AIB,l);
-  analogWrite(BIA,0);
-  analogWrite(BIB,r);
-  delay(t);
-}
-
-void reverse(int l, int r, int t)
-{
-  analogWrite(AIA,l);
-  analogWrite(AIB,0);
-  analogWrite(BIA,r);
-  analogWrite(BIB,0);
-  delay(t);
 }
