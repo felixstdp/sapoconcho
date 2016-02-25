50f2434c923de23922d2225a59487251766f63b3
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-02-25 13:03:34 +0100

    Update L9110.cpp

--- L9110.cpp
+++ L9110.cpp
@@ -1,10 +1,51 @@
 #include <Arduino.h>
 #include "L9110.h"
 
-#define AIA 10
-#define AIB 11
-#define BIA 6
-#define BIB 9
+/*
+            < ARDUINO PRO MICRO >
+             
+                |TX0   RAW| Batteries V+
+                |RX1   GND| Batteries GND
+                |GND   RST|
+                |GND   VCC| Breadboard 5V
+                |2     #A3| 
+      L9110 BIA |3~    #A2| 
+                |4#    #A1| 
+      L9110 BIB |5~    #A0| 
+      L9110 AIA |6~#    15|
+                |7      14| 
+                |8#     16| 
+      L9110 AIB |9~#  #~10| 
+~ PWM
+# Analog
+*/
+
+/*
+              < ARDUINO MICRO >
+               
+                |MOSI  SCK|
+                |SS   MISO|
+                |TX0   VIN| Batteries V+
+                |TX1   GND| Batteries GND
+                |RST   RST| Reset
+                |GND    5V| Breadboard 5V
+                |2        | 
+                |3~       | 
+                |4     #A5| 
+                |5~    #A4| 
+                |6~    #A3| 
+                |7     #A2|
+                |8     #A1| 
+      L9110 BIA |9~    #A0| 
+      L9110 BIB |10~  AREF| 
+      L9110 AIA |11~   3V3| 
+      L9110 AIB |12~   ~13| 
+*/
+
+#define AIA 11 // cambiar según esquema superior si se usa Arduino Pro Micro
+#define AIB 12
+#define BIA 9
+#define BIB 10
 
 L9110::L9110(){
   pinMode(AIA,OUTPUT);
