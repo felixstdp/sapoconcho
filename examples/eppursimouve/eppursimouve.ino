005f2d7d5299420fb87c951966f29fe2211bfcf0
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-02-25 11:52:21 +0100

    Update eppursimouve.ino

--- examples/eppursimouve/eppursimouve.ino
+++ examples/eppursimouve/eppursimouve.ino
@@ -1,19 +1,9 @@
 
-#include <sapoconcho.h>
-sapoconcho beta1;
-
-#define AIA 9 // HIGH >> left backward
-#define AIB 10 // HIGH >> left forward
-#define BIA 12 // HIGH >> right forward
-#define BIB 11 // HIGH >> right backwards
+#include <L9110.h>
+L9110 beta1;
 
 void setup() {
 
-  pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
-  
 }
 
 void loop() {
