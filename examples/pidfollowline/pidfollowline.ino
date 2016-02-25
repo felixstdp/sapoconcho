a62496dabe5fb8d410742d8627ec725f13cceebb
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-02-25 11:54:42 +0100

    Update pidfollowline.ino

--- examples/pidfollowline/pidfollowline.ino
+++ examples/pidfollowline/pidfollowline.ino
@@ -1,3 +1,6 @@
+#include <L9110.h>
+L9110 sapoconcho;
+
 // siguelíneas con algoritmo PID
 // utiliza seis sensores analógicos en la parte frontal
 // tres a la izquierda y tres a la derecha
@@ -11,19 +14,8 @@ float i=0;
 float p_old=0;
 int u;
 
-#define AIA 11 // HIGH >> left backward
-#define AIB 6 // HIGH >> left forward
-#define BIA 9 // HIGH >> right forward
-#define BIB 10 // HIGH >> right backwards
 
-void setup() {
-
-  pinMode(AIA,OUTPUT);
-  pinMode(AIB,OUTPUT);
-  pinMode(BIA,OUTPUT);
-  pinMode(BIB,OUTPUT);
-  
-}
+void setup() {}
 
 void loop()
 {
@@ -35,33 +27,5 @@ void loop()
   if ((p*i)<0) i=0;  // corrige el overshooting - integral windup
 
   u=kp*p+ki*i+kd*d;
-  drive(forward-u,forward+u,0);
-}
-
-void drive(int l, int r, int t)
-{
-  if (l>255) l=255;
-  if (l<-255) l=-255;
-  if (r>255) r=255;
-  if (r<-255) r=-255; 
-  
-  if (l>0)
-  {
-    analogWrite(AIA,0);
-    analogWrite(AIB,l);
-  } else {
-    analogWrite(AIA,-l);
-    analogWrite(AIB,0);
-  }
-  
-  if (r>0)
-  {
-    analogWrite(BIA,0);
-    analogWrite(BIB,r);    
-  } else {
-    analogWrite(BIA,-r);
-    analogWrite(BIB,0);
-  }
-  
-  delay(t);
+  sapoconcho.drive(forward-u,forward+u,0);
 }
