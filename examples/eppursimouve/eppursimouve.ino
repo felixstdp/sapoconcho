2eee706c1f860be98b7bef40bf07e417a97fd3a1
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-02-25 13:13:52 +0100

    Update eppursimouve.ino

--- examples/eppursimouve/eppursimouve.ino
+++ examples/eppursimouve/eppursimouve.ino
@@ -1,17 +1,14 @@
-
 #include <L9110.h>
-L9110 beta1;
+L9110 sapoconcho;
 
-void setup() {
-
-}
+void setup() {}
 
 void loop() {
 
-  beta1.forward(128,128,1000);
-  beta1.reverse(128,128,1000);
-  beta1.rotate(1000);
-  beta1.rotate(-1000);
-  beta1.brake(1000);
+  sapoconcho.forward(128,128,1000);
+  sapoconcho.reverse(128,128,1000);
+  sapoconcho.rotate(1000);
+  sapoconcho.rotate(-1000);
+  sapoconcho.brake(1000);
 
 }
