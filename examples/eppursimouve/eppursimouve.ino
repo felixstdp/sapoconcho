9362c54aeebe8fa7a4ee1269e38ad35b5d6472fb
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-03-07 21:34:15 +0100

    Update eppursimouve.ino

--- examples/eppursimouve/eppursimouve.ino
+++ examples/eppursimouve/eppursimouve.ino
@@ -7,8 +7,8 @@ void loop() {
 
   sapoconcho.forward(128,128,1000);
   sapoconcho.reverse(128,128,1000);
-  sapoconcho.rotate(1000);
-  sapoconcho.rotate(-1000);
+  sapoconcho.rotate(128,128,1000);
+  sapoconcho.rotate(128,128,-1000);
   sapoconcho.brake(1000);
 
 }
