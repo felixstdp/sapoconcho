12f3cd0cd39d5ad1ff18203a625139726263f804
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-11-11 22:21:41 +0100

    Update siguelineas.ino

--- examples/siguelineas/siguelineas.ino
+++ examples/siguelineas/siguelineas.ino
@@ -1,11 +1,11 @@
 #include <L9110.h>
-L9110 sapoconcho;
+L9110 sapoconcho(3,5,6,9);
 
 void setup() {}
 
 void loop()
 {
-  if (analogRead(A2)<500) sapoconcho.rotate(150,150,100);
-  if (analogRead(A3)<500) sapoconcho.rotate(150,150,-100);
+  if (analogRead(A0)<500) sapoconcho.rotate(150,150,100);
+  if (analogRead(A1)<500) sapoconcho.rotate(150,150,-100);
   sapoconcho.forward(150,150,100);
 }
