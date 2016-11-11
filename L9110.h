113230df368b8ea56a1c31a9ee4bfd452b63ca1d
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-11-11 22:16:21 +0100

    Update L9110.h

--- L9110.h
+++ L9110.h
@@ -3,11 +3,17 @@
 
 class L9110 {
   public:
-    L9110();
+    L9110(int aia, int aib, int bia, int bib);
     void forward(int l, int r, int t);
     void reverse(int l, int r, int t);
     void rotate(int l, int r, int t);
     void brake(int t);
     void drive(int l, int r, int t);
-};
+  private:
+    int _aia;
+    int _aib;
+    int _bia;
+    int _bib;
+}; 
+
 #endif
