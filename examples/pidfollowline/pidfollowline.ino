a063c46f946363fbfb52f16e5e36f526e0b4a52f
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2016-12-02 19:22:50 +0100

    Update pidfollowline.ino

--- examples/pidfollowline/pidfollowline.ino
+++ examples/pidfollowline/pidfollowline.ino
@@ -13,20 +13,18 @@ int forward=120;
 float kp=.08;
 float ki=0.03;
 float kd=0.0;
-float p,d;
+int p,d;
 float i=0;
 float p_old=0;
 int u;
-
 
 void setup() {}
 
 void loop()
 {
-
   qtra.read(IR); // read raw sensor values
 
-  p = -3*IR[0]-2*IR[1]-IR[2]+IR[3]+2*IR[4]+3*IR[4];
+  p = -3*IR[0]-2*IR[1]-IR[2]+IR[3]+2*IR[4]+3*IR[5];
 
   i=i+p;
   d=p-p_old;
@@ -35,5 +33,8 @@ void loop()
   if ((p*i)<0) i=0;  // corrige el overshooting - integral windup
 
   u=kp*p+ki*i+kd*d;
-  sapoconcho.drive(forward-u,forward+u,0);
+  sapoconcho.drive(forward+u,forward-u,0);
+
+//  Serial.println(p);
+  
 }
