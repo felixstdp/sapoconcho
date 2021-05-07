b845ef1aa6c3866720a88b99ecd2c8ca1fbdc2a1
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2021-05-07 09:23:40 +0200

    Create sapoconchoL.h

---
+++ motores_amarillos/sapoconchoL.h
@@ -0,0 +1,19 @@
+#ifndef sapoconchoL
+#define sapoconchoL
+
+class L9110 {
+  public:
+    L9110(int ain1, int bin1, int pwma, int pwmb);
+    void forward(int l, int r, int t);
+    void reverse(int l, int r, int t);
+    void rotate(int l, int r, int t);
+    void brake(int t);
+    void drive(int l, int r, int t);
+  private:
+    int _ain1;
+    int _bin1;
+    int _pwma;
+    int _pwmb;
+}; 
+
+#endif
